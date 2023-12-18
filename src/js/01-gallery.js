8eba714d3f455d99ffaa1a180f7beb3f693ca0d1
Neelkaa96 <neelka96@gmail.com>
2023-12-18 21:35:58 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const { log } = console;
+const gallery = document.querySelector('.gallery');
+let lightbox;
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('gallery__image')) {
+    lightbox = new SimpleLightbox('.gallery .gallery__item', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+    lightbox.open();
+  }
+});
+[...galleryItems].forEach(photo => {
+  let galleryItem = document.createElement('a');
+
+  galleryItem.classList.add('gallery__item');
+  galleryItem.setAttribute('href', `${photo.original}`);
+  
+  galleryItem.insertAdjacentHTML(
+    'afterbegin',
+    `<img class="gallery__image" src="${photo.preview}" data-source="${photo.original}" alt="${photo.description}">`
+  );
+
+  gallery.appendChild(galleryItem);
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 
+player.on(
+  'timeupdate',
+  _.throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+let currentTime = 0;
+
+if (localStorage.getItem('videoplayer-current-time') != null) {
+  currentTime = localStorage.getItem('videoplayer-current-time');
+}
+
+player.setCurrentTime(currentTime);
